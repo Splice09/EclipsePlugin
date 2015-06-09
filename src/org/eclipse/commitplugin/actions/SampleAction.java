e49e11dcd2fc99e0eea01e413a2ebd55673c49be
Splice09 <jpf0005@gmail.com>
2015-06-08 20:58:38 -0500

    Add ok and cancel button and center plugin window popup

--- src/org/eclipse/commitplugin/actions/SampleAction.java
+++ src/org/eclipse/commitplugin/actions/SampleAction.java
@@ -12,6 +12,8 @@ import org.eclipse.swt.layout.*;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
 
 /**
  * Our sample action implements workbench action delegate.
@@ -45,17 +47,35 @@ public class SampleAction implements IWorkbenchWindowActionDelegate{
 	public void run(IAction action) {
 		//create shell object
 		Shell shell = new Shell(window.getShell());
-		//create layout object
-		RowLayout layout = new RowLayout();
-		//set layout margins
-        layout.marginLeft = 50;
-        layout.marginTop = 50;
+		
+		//create layout object and set shell layout
+		FormLayout layout = new FormLayout();
         shell.setLayout(layout);
-        //create new button with cancel label
+        
+        //create new buttons with cancel and ok labels
+        Button okBtn = new Button(shell, SWT.PUSH);
+        okBtn.setText("OK");
         Button quitBtn = new Button(shell, SWT.PUSH);
         quitBtn.setText("Cancel");
-        quitBtn.setLayoutData(new RowData(80, 30));
-        //create listener for button press
+        
+        //positon the two buttons
+        FormData okData = new FormData(80, 30);
+        okData.right = new FormAttachment(98);
+        okData.bottom = new FormAttachment(95);
+        okBtn.setLayoutData(okData);
+
+        FormData cancelData = new FormData(80, 30);
+        cancelData.right = new FormAttachment(okBtn, -5, SWT.LEFT);
+        cancelData.bottom = new FormAttachment(okBtn, 0, SWT.BOTTOM);
+        quitBtn.setLayoutData(cancelData);
+        
+        //create listener for button presses
+        okBtn.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                shell.setVisible(false);
+            }
+        });
         quitBtn.addSelectionListener(new SelectionAdapter() {
             @Override
             public void widgetSelected(SelectionEvent e) {
@@ -64,7 +84,8 @@ public class SampleAction implements IWorkbenchWindowActionDelegate{
         });
         //set shell title, and size
         shell.setText("Git Commit Plugin");
-        shell.setSize(250, 200);
+        shell.setSize(350, 200);
+        centerWindow(shell);
         //open shell
         shell.open();
 
@@ -97,4 +118,19 @@ public class SampleAction implements IWorkbenchWindowActionDelegate{
 	public void init(IWorkbenchWindow window) {
 		this.window = window;
 	}
+	
+	/*
+	 * This method centers the plugin window on the screen upon open.
+	*/
+	private void centerWindow(Shell shell){
+		
+		Rectangle bds = shell.getDisplay().getBounds();
+
+        Point p = shell.getSize();
+
+        int nLeft = (bds.width - p.x) / 2;
+        int nTop = (bds.height - p.y) / 2;
+
+        shell.setBounds(nLeft, nTop, p.x, p.y);
+    }
 }
